44e943bf55a70e97dcac58df1d3c95ffc1864288
mortari17 <leomortari15@gmail.com>
2026-08-26 08:28:17 -0300

    Adicionando TarefaRepository e TarefaViewModel

---
+++ app/src/main/java/Feteixeira/com/github/todolist/repository/TarefaRepository.kt
@@ -0,0 +1,16 @@
+package Feteixeira.com.github.todolist.repository
+
+import Feteixeira.com.github.todolist.data.Tarefa
+import Feteixeira.com.github.todolist.data.TarefaDao
+import kotlinx.coroutines.flow.Flow
+
+class TarefaRepository(private val dao: TarefaDao) {
+
+    val tarefas: Flow<List<Tarefa>> = dao.listarTodas()
+
+    suspend fun inserir(tarefa: Tarefa) = dao.inserir(tarefa)
+
+    suspend fun atualizar(tarefa: Tarefa) = dao.atualizar(tarefa)
+
+    suspend fun deletar(tarefa: Tarefa) = dao.deletar(tarefa)
+}
